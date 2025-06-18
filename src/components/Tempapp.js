59d2dffcaeb146bf62295351afeef6a875dd3e53
unknown <muktinathrajbanshi786@gmail.com>
2025-06-18 14:01:21 +0545

    just adding style through css for box design

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -11,11 +11,25 @@ const Tempapp = () => {
                     type="search"
                     className="inputField" 
                     onChange={ (event) => {
-
                     } }    
                     />
                 </div>
+           
+            <div className="info">
+                <h2 className="location"> 
+                <i class="fa-solid fa-street-view"></i>Kathmandu
+                 </h2>
+                 <h1 className="temp">
+                    5.25°Cel
+                 </h1>
+                 <h3 className="tempmin_max"> Min : 5.25°Cel | Max : 5.25°Cel </h3>
             </div>
+
+            <div className="wave -one"></div>
+            <div className="wave -two"></div>
+            <div className="wave -three"></div>
+
+             </div>
         </>
     );
 };
